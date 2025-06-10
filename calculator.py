2b9abfc54d7e2426708dec074be2cf9b1ce19728
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-06-10 14:00:22 +0530

    Add input, calculation, and error handling functions

--- calculator.py
+++ calculator.py
@@ -3,3 +3,32 @@ from tkinter import font
 from tkinter import Label
 from functools import partial
 
+def get_input(entry, argu):
+    entry.insert(END, argu)
+
+def backspace(entry):
+    input_len = len(entry.get())
+    entry.delete(input_len - 1)
+
+def clear(entry):
+    entry.delete(0, END)
+
+def calc(entry):
+    input_info = entry.get()
+    try:
+        output = str(eval(input_info.strip()))
+    except ZeroDivisionError:
+        popupmsg()
+        output = ""
+    clear(entry)
+    entry.insert(END, output)
+
+def popupmsg():
+    popup = Tk()
+    popup.resizable(0, 0)
+    popup.geometry("120x100")
+    popup.title("Alert")
+    label = Label(popup, text="Cannot divide by 0 ! \n Enter valid values")
+    label.pack(side="top", fill="x", pady=10)
+    B1 = Button(popup, text="Okay", bg="#DDDDDD", command=popup.destroy)
+    B1.pack()
